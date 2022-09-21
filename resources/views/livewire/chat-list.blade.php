987ca22002bcb377c12a7b139d682f7be4f10b7f
JaviMY <javier@masteryield.com>
2022-09-21 13:24:26 +0200

    Hablanco -> Hablando

--- resources/views/livewire/chat-list.blade.php
+++ resources/views/livewire/chat-list.blade.php
@@ -6,7 +6,7 @@
             <div class="grid grid-cols-7 gap-4 px-4 py-2 bg-blue-700 text-white font-semibold rounded-t-lg border-b-2 content-center align-middle">
                 <span class="self-center col-span-2">Servicio</span>    
                 <span class="self-center col-span-2">Creador</span>    
-                <span class="self-center col-span-2">Hablanco con</span>    
+                <span class="self-center col-span-2">Hablando con</span>    
             </div>
             @foreach ( $chats as $chat )
                 <x-chat-item-list :chat="(object)$chat"></x-chat-item-list>
