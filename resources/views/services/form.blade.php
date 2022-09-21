3a3e38e3f2c936f44336576efe8d7377d5d86db1
JaviMY <javier@masteryield.com>
2022-09-21 16:09:40 +0200

    Individual errors for service create form

--- resources/views/services/form.blade.php
+++ resources/views/services/form.blade.php
@@ -1,13 +1,4 @@
 <x-app-layout>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -22,14 +13,17 @@
             <div class="mb-6">
                 <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Título</label>
                 <input type="text" name="title" id="title" value="{{ old('title') }}" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Clases de guitarra, Masajes..." required="">
+                @error('title') <span class="text-red-600">{{ $message }}</span> @enderror
             </div>
             <div class="mb-6">
                 <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Descripción</label>
                 <textarea rows="4" cols="50" name="description" id="description" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Me dedico a esto desde hace nuchos años..." required="">{{ old('description') }}</textarea>
+                @error('description') <span class="text-red-600">{{ $message }}</span> @enderror
             </div>
             <div class="mb-6">
                 <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Precio</label>
                 <input type="number" name="price" id="price" value="{{ old('price') }}" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required="">
+                @error('price') <span class="text-red-600">{{ $message }}</span> @enderror
             </div>
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear servicio</button>
         </form>
